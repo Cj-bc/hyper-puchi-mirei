2f24c135061b4bc1d57c0ef2000e76e264306364
Cj-bc <cj-bc@y-modify.org>
2018-07-18 11:57:29 +0900

    Fix: change imange path

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ exports.decorateTerm = (Term, { React, notify }) => {
     return class extends React.Component {
         render () {
             const children = [
-                 React.createElement("img", {style: styles.img, src: "/Users/coju/.hyper_plugins/local/mirei/img/puch-mirei.png"}),
+                 React.createElement("img", {style: styles.img, src: "/Users/coju/.hyper_plugins/local/hyper-puchi-mirei/img/puch-mirei.png"}),
                  React.createElement(Term, this.props),
             ];
             return React.createElement("div", {style: { width: '100%', height: '100%', position: 'relative' }}, children);
